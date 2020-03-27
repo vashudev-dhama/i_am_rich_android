f92a0f2dcece92515703d84fb3815dc4e3b7f58f
vashudev-dhama <dhamavashu1119@gmail.com>
2020-03-27 12:47:47 +0530

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        title: 'App title',
+        title: 'I am Rich',
         home: Scaffold(
           backgroundColor: Colors.blueGrey,
           appBar: AppBar(
-            title: Text('Screen title!'),
+            title: Text('I am Rich'),
             backgroundColor: Colors.blueGrey[900],
           ),
           body: Center(
